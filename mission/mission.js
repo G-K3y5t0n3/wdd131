3b808931bf7ebc1ec523d75acbbf3f491a3b2f71
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-05-05 17:48:33 -0600

    week 2 prove

--- mission/mission.js
+++ mission/mission.js
@@ -1,16 +1,23 @@
-const themeSelector = // replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
 function changeTheme() {
-// check to see what the current value of our select is.
-// The current value is conveniently found in themeSelector.value!
+    let current = selectElem.value;
 
-// if the value is dark then:
-// add the dark class to the body
-// change the source of the logo img to point to the white logo.
-// otherwise
-// remove the dark class
-// make sure the logo src is the blue logo.
+    if (current == 'dark'){
+        // give body the dark class
+        document.body.classList.add('dark')
+        // add different image by changing src
+        logo.src = 'images/byui-logo_white.jpg';
+
+
+    } else {
+        // take off class
+        document.body.classList.remove("dark");
+        // change the image back to original
+        logo.src = 'images/byui-logo_blue.jpg';
+
+    }
 }
-
-// add an event listener to the themeSelector element here.
-// Use the changeTheme function as the event handler function.
-themeSelector.addEventListener('change', changeTheme);
--- ponder/dom-intro/dom-basics.js
+++ ponder/dom-intro/dom-basics.js
@@ -7,11 +7,24 @@ newImage.setAttribute("src", "https://picsum.photos/200");
 newImage.setAttribute("alt", "Random image from picsum.");
 document.body.appendChild(newImage);
 
-/* const newDiv - document.createElement("div");
+const newDiv = document.createElement("div");
 newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
 document.body.appendChild(newDiv);
-*/
 
-/* const newSection = document.createElement('section');
-newSection.
-*/
+// Step 4 Solution 1
+
+// const newSection = document.createElement("section");
+// const newH2 = document.createElement("h2");
+// newH2.innerText = "DOM Basics";
+// newSection.appendChild(newH2);
+// const newP = document.createElement("p");
+// newP.innerText = "This was added through Javascript";
+// newSection.appendChild(newP);
+
+// document.body.appendChild(newSection);
+
+//Step 4 Solution 2
+
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
+document.body.appendChild(newSection);
