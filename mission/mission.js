c7424b646efba31052d10e1125916ece6379bfac
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-05-07 21:11:26 -0600

    week2-prove-revision

--- mission/mission.js
+++ mission/mission.js
@@ -8,7 +8,8 @@ function changeTheme() {
 
     if (current == 'dark'){
         // give body the dark class
-        document.body.classList.add('dark')
+        document.body.classList.add('dark');
+        document.querySelector('.subtitle').classList.add('dark-subtitle');
         // add different image by changing src
         logo.src = 'images/byui-logo_white.jpg';
     }
@@ -16,6 +17,7 @@ function changeTheme() {
     else {
         // take off class
         document.body.classList.remove("dark");
+        document.querySelector('.subtitle').classList.remove('dark-subtitle');
         // change the image back to original
         logo.src = 'images/byui-logo_blue.jpg';
     }
