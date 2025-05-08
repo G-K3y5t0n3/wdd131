fb0e021f90fbb39fc9b0c1d7f6318218d59eb28a
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-05-07 22:18:33 -0600

    prove-media-queries-progress

---
+++ coolpics/coolpics.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('header button');
+    const menu = document.getElementById('menu');
+  
+    menuButton.addEventListener('click', () => {
+      menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
+    });
+  });
+  
