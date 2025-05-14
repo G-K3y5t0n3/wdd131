553c21e5c294268bb78a0c0f80250a8ff3f518cc
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-05-14 12:17:24 -0600

    coolpics and debugging ponder

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuButton = document.querySelector('header button');
-    const menu = document.getElementById('menu');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const menuButton = document.querySelector('header button');
+//     const menu = document.getElementById('menu');
   
-    menuButton.addEventListener('click', () => {
-      menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
-    });
-  });
+//     menuButton.addEventListener('click', () => {
+//       menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
+//     });
+//   });
   
---
+++ ponder/debugging/debugging.js
@@ -0,0 +1,45 @@
+// const PI = 3.14;
+// const radius = 3;
+// let area = 0;
+// area = radius * radius * pi;
+// radius = 4;
+// area = radius * radius * pi;
+
+
+// Activity 1 Solution
+// const PI = 3.14;
+// let radius = 3;
+// let area = 0;
+// area = radius * radius * PI;
+// console.log("Area1:", area);
+// radius = 4;
+// area = radius * radius * PI;
+// console.log("Area2:", area);
+
+// function circleArea(radius) {
+//   // code to complete our task here
+// }
+
+// const PI = 3.14;
+// let area = 0;
+// function circleArea(radius) {
+//   const area = radius * PI;
+// }
+// area = circleArea(3);
+// console.log(area);
+
+// Activity 2 Solution
+// const PI = 3.14;
+// // let radius = 3;
+// let area = 0;
+
+// function circleArea(radius) {
+//   const area = radius * radius * PI;
+//   return area;
+// }
+
+// area = circleArea(3);
+// console.log("Area1:", area);
+// // radius = 4;
+// area = circleArea(4);
+// console.log("Area2:", area);
