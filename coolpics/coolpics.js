ded588bc8d4eaacc079388edac86d6c67334b690
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-05-29 17:57:44 -0600

    coolpics progress

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -17,3 +17,29 @@ function handleResize() {
 
 handleResize();
 window.addEventListener("resize", handleResize);
+
+function getBiggerImage(path) {
+  const url = new URL(path);
+  const newPath = url.pathname.split("-")[0];
+  return newPath + '-full.jpg';   
+}
+
+const imageViewer = document.querySelector("dialog")
+
+document.querySelector(".gallery").addEventListener("click", (event) => {
+  if (event.target.tagName !== "IMG") return;
+
+  const fullImage = imageViewer.querySelector("img");
+  fullImage.src = getBiggerImage(event.target.src) 
+  fullImage.alt = event.target.alt
+
+  imageViewer.showModal()
+})
+
+imageViewer.addEventListener("click", (event) => {
+  if (event.target.tagName === "DIALOG" ||
+    event.target.tagName === "BUTTON"    
+  )  {
+    imageViewer.close()
+  }
+})
